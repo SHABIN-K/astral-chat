78dcec8b2fcf43956bbc6eb040a8b213a278c10d
Shabin-k <helloshabink@gmail.com>
2023-12-24 21:48:42 +0530

    fix : style

--- packages/menuclick/components/web/Footer.jsx
+++ packages/menuclick/components/web/Footer.jsx
@@ -5,10 +5,10 @@ import { FaInstagram, FaTwitter, FaGithub, FaTelegram } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="left-0 bottom-0 w-full fixed h-[3rem] border-t-2 border-black dark:border-gray-50 flex items-center justify-between bg-white dark:bg-black">
-      <Link href="/" className="ml-5 pl-5 font-semibold text-lg ">
+      <Link href="/" className=" ml-5 pl-5 font-semibold text-lg ">
         <h1>MenuCLick</h1>
       </Link>
-      <div className="flex gap-4 mr-5 items-center h-full">
+      <div className="flex gap-4 mr-5 items-center h-full pr-5">
         <Link href="https://twitter.com/Shabink9/" target="_blank">
           <FaTwitter size={25} />
         </Link>
