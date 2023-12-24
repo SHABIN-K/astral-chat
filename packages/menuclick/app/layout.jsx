a7b8e394c50569d997138059b19942991b5ab3ee
Shabin-k <helloshabink@gmail.com>
2023-12-24 12:39:22 +0530

    feat : add toaster

--- packages/menuclick/app/layout.jsx
+++ packages/menuclick/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ColorProvider from "@/modules/providers/ColorProvider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <ColorProvider>
+        <Toaster />
         <body className={inter.className}>{children}</body>
       </ColorProvider>
     </html>
