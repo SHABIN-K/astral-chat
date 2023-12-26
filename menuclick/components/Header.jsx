6b47f4c7258f13a37959e819b726da6c4d8070b3
Shabin-k <helloshabink@gmail.com>
2023-12-26 18:20:29 +0530

    bug

--- menuclick/components/Header.jsx
+++ menuclick/components/Header.jsx
@@ -2,9 +2,9 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 
 import Hamburger from "./ui/Hamburger";
 import { Avathar, Logo } from "@/public/assets";
@@ -13,12 +13,16 @@ import ThemeSwitcher from "./ui/ThemeSwitcher";
 
 const Header = () => {
   const pathname = usePathname();
-
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive);
   return (
     <nav className="bg-color border-b-2 border-color rounded-b-lg">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <div
+            className="absolute inset-y-0 left-0 flex items-center sm:hidden"
+            onClick={() => setIsActive((prevIsActive) => !prevIsActive)}
+          >
             <Hamburger />
           </div>
           <div className="flex-center flex-1 sm:justify-start">
@@ -88,6 +92,27 @@ const Header = () => {
           </div>
         </div>
       </div>
+
+      {isActive && (
+        <div className="sm:hidden">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            {navlinks.map((item, index) => (
+              <Link
+                key={index}
+                href={item.link}
+                className={`rounded-lg px-5 py-2 ${
+                  pathname === item.link && "border-2 border-color"
+                } `}
+                onClick={() => setIsActive(false)}
+              >
+                <span className="text-color text-md font-medium">
+                  {item.name}
+                </span>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
